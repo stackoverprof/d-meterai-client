d6912a3f51ecf02df0f71531edd6016c475ae567
stackoverprof <mail.errbint@gmail.com>
2022-10-07 14:11:03 +0700

    feat: admin area navbar #2

--- components/NavigationBar/NavbarAdmin.tsx
+++ components/NavigationBar/NavbarAdmin.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ConnectionPortal from './ConnectionPortal';
 import { RiShieldFlashLine } from 'react-icons/ri';
+import { MdOutlineHome } from 'react-icons/md';
+import Link from '@components/_shared/Link';
 
 const NavbarAdmin = () => {
 	return (
@@ -13,7 +15,15 @@ const NavbarAdmin = () => {
 						<p className="text-2xl font-bold text-theme-purple">d-Meterai </p>
 					</div>
 				</div>
-				<ConnectionPortal />
+				<div className="flex-cc gap-2">
+					<ConnectionPortal />
+					<Link
+						href="/"
+						className="flex-cc w-11 h-11 bg-white bg-opacity-0 rounded hover:bg-opacity-40 "
+					>
+						<MdOutlineHome className="w-8 h-8" />
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
--- components/NavigationBar/NavbarMain.tsx
+++ components/NavigationBar/NavbarMain.tsx
@@ -1,7 +1,9 @@
 import Logo from '@components/NavigationBar/Logo';
+import Link from '@components/_shared/Link';
 import useIsOwner from '@core/hooks/useIsOwner';
 import React from 'react';
-import AdminButton from './AdminButton';
+import { RiShieldFlashLine } from 'react-icons/ri';
+
 import ConnectionPortal from './ConnectionPortal';
 
 const NavbarMain = () => {
@@ -12,7 +14,14 @@ const NavbarMain = () => {
 				<Logo />
 				<div className="flex-cc gap-2">
 					<ConnectionPortal />
-					{isOwner && <AdminButton />}
+					{isOwner && (
+						<Link
+							href="/admin"
+							className="flex-cc w-11 h-11 bg-white bg-opacity-0 rounded hover:bg-opacity-40 "
+						>
+							<RiShieldFlashLine className="w-8 h-8" />
+						</Link>
+					)}
 				</div>
 			</div>
 		</div>
