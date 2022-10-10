66b717673a8ab605ddfa87b08ff1a44db1dc19e9
stackoverprof <mail.errbint@gmail.com>
2022-10-10 18:05:14 +0700

    feat: qr generate and upload

--- components/Pembubuhan/Uploader.tsx
+++ components/Pembubuhan/Uploader.tsx
@@ -1,23 +1,29 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Web3Storage } from 'web3.storage';
+import qr from 'qr-image';
+import { useRouter } from 'next/router';
 
-// Construct with token and endpoint
 const client = new Web3Storage({ token: process.env.NEXT_PUBLIC_WEB3_STORAGE_API_TOKEN });
-
-// const fileInput = document.querySelector('input[type="file"]');
-
-// Pack files into a CAR and send to web3.storage
-
-// Get info on the Filecoin deals that the CID is stored in
-
-// Fetch and verify files from web3.storage
 
 const Uploader = () => {
 	const [fileUrl, setFileUrl] = useState('');
-	const [file, setFile] = useState(null);
+	const [file, setFile] = useState<any[]>(null);
+
+	const router = useRouter();
+	const { tokenId } = router.query;
+
+	const generateQR = () => {
+		const verificationURL = `${window.location.origin}/verify?tokenId=${tokenId}`;
+		const buffer = qr.imageSync(verificationURL, { type: 'png' });
+		const blob = new Blob([buffer], { type: 'image/png' });
+		const blobFile = new File([blob], 'qr-stamp.png');
+		return blobFile;
+	};
 
 	const handleUpload = async () => {
-		const rootCid = await client.put(file);
+		const qrStamp = generateQR();
+		if (!file || !file.length) return;
+		const rootCid = await client.put([...file, qrStamp]);
 		setFileUrl(`https://${rootCid}.ipfs.w3s.link`);
 	};
 
@@ -27,13 +33,25 @@ const Uploader = () => {
 		setFile(file);
 	};
 
+	// console.log('file', file);
+	// const router = useRouter();
+	// const { tokenId } = router.query;
+
+	// const handleQR = async () => {
+	// 	console.log('blob', blob);
+	// 	setFile((prev) => [...prev, blobFile]);
+	// 	document.getElementById('my-img').src = URL.createObjectURL(blob);
+	// };
+
+	// console.log('file', file);
+
 	return (
 		<div className="flex-sc col">
 			<div className="w-[800px] mb-8 h-48 flex-cc rounded border-2 relative border-theme-purple">
 				<input type="file" id="file" className="opacity-0" onChange={handleFileChange} />
 				<label
 					htmlFor="file"
-					className="absolute flex-cc text-xl bg-white bg-opacity-0 hover:bg-opacity-20 full"
+					className="absolute flex-cc z-50 text-xl bg-white bg-opacity-0 hover:bg-opacity-20 full"
 				>
 					{file === null ? 'Letakkan file di sini' : `File: ${file[0].name}`}
 				</label>
